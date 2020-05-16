8c76fd87713d1120a690f80282fd3f92b1a34d56
gaonan <gaonanlvmm@gmail.com>
2020-05-16 21:16:41 +0800

    update

--- MediatRDemo/Program.cs
+++ MediatRDemo/Program.cs
@@ -1,12 +1,53 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace MediatRDemo
 {
     class Program
     {
-        static void Main(string[] args)
+        async static Task Main(string[] args)
         {
+            var services = new ServiceCollection();
+
+            services.AddMediatR(typeof(Program).Assembly);
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            var mediator = serviceProvider.GetService<IMediator>();
+
+            await mediator.Send(new MyCommand { CommandName = "cmd01" });
+
             Console.WriteLine("Hello World!");
         }
+
+        internal class MyCommand : IRequest<long>
+        {
+            public string CommandName { get; set; }
+        }
+
+        internal class MyCommandHandlerV2 : IRequestHandler<MyCommand, long>
+        {
+
+            public Task<long> Handle(MyCommand request, CancellationToken cancellationToken)
+            {
+                Console.WriteLine($"MycommandHandleV2执行命令:{request.CommandName}");
+
+                return Task.FromResult(10L);
+            }
+        }
+        internal class MyCommandHandler : IRequestHandler<MyCommand,long>
+        {
+            public Task<long> Handle(MyCommand request, CancellationToken cancellationToken)
+            {
+                Console.WriteLine($"MycommandHandle执行命令:{request.CommandName}");
+
+                return Task.FromResult(10L);
+            }
+          
+        }
+       
     }
 }
